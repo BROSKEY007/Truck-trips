752088edc2ee732be0b252e3d156ce92a354b5e8
BROSKEY007 <90440485+BROSKEY007@users.noreply.github.com>
2023-06-13 13:33:12 +0600

    Update inputgen.cpp

--- inputgen.cpp
+++ inputgen.cpp
@@ -1,1 +1,16 @@
 # Truck-trips
+class RandomInputGen{
+    int numPlaces;
+    public:
+    RandomInputGen(int numPlaces){
+        this->numPlaces = numPlaces;
+        genDist();
+    }
+    void genDist(){
+        for(int p1 = 0; p1 <= numPlaces; p1++){
+            for(int p2 = 0; p2 <= numPlaces; p2++){
+                if(p1 == p2) continue;
+                getDistance(p1, p2);
+            }
+        }
+    }
